1b92dde3f0f8174ab7c6d52c834e175229fa6861
Emine Shaban <emineshaban@icloud.com>
2023-09-25 15:45:04 +0300

    .

--- src/Main7.java
+++ src/Main7.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main6 {
+public class Main7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          int row = Integer.parseInt(sc.nextLine());
@@ -20,7 +19,12 @@ public class Main6 {
         }
 
 
+        for (int rows = 0; rows < row; rows++) {
+            for (int cols = 0; cols < row; cols++) {
 
+
+            }
+        }
 
         for (int i = 0; i < row; i++) {
 
